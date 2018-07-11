dcc50082781f1adde03971d66c952d652fa6f912
philippe.beroucry <philippe.beroucry.prestataire@it-ce.fr>
2018-07-11 23:21:10 +0200

    add missing module

--- projects/ngx-phone-number/src/lib/ngx-phone-number.module.ts
+++ projects/ngx-phone-number/src/lib/ngx-phone-number.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { NgxPhoneNumberComponent } from './ngx-phone-number.component';
 import {HttpClientModule} from '@angular/common/http';
 import {CommonModule} from '@angular/common';
-import {MatAutocompleteModule, MatFormFieldModule, MatInputModule, MatOptionModule} from '@angular/material';
+import {MatAutocompleteModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatOptionModule} from '@angular/material';
 
 @NgModule({
   imports: [
@@ -11,7 +11,8 @@ import {MatAutocompleteModule, MatFormFieldModule, MatInputModule, MatOptionModu
     MatAutocompleteModule,
     MatOptionModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatButtonModule
   ],
   declarations: [NgxPhoneNumberComponent],
   exports: [NgxPhoneNumberComponent]
